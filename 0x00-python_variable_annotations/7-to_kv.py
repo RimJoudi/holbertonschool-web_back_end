b7c10212934bc52bf29e215fb9f8cbfe8b504489
RimJoudi <joudirim93@gmail.com>
2022-06-12 23:38:12 +0100

    fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,10 +8,10 @@ int/float v and should be annotated as a float.
 """
 
 
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     type-annotated function to_kv that takes a string k
     and an int OR float v as arguments and returns a tuple.
