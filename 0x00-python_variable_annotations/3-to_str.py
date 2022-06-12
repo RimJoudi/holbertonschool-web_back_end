33c0b003e36ddd992be1507e45562b557af14726
RimJoudi <joudirim93@gmail.com>
2022-06-12 22:05:29 +0100

    fix

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-"""
-function that takes a float n as argument 
-and returns the string representation of the float.
-"""
+""" a type-annotated function to_str """
 
 
 def to_str(n: float) -> str:
     """
-    function that takes a float n as argument 
+    a type-annotated function to_str that
+    takes a float n as argument
     and returns the string representation of the float.
     """
-
     return str(n)
