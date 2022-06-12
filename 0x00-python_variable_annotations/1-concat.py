2222e107452616df0095e5236a643931f15245a4
RimJoudi <joudirim93@gmail.com>
2022-06-12 22:31:22 +0100

    add tasks

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-type-annotated function concat that takes 
+type-annotated function concat that takes
 str1 and str2 as arguments and
 returns a concatenated string
 
@@ -8,5 +8,11 @@ returns a concatenated string
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    type-annotated function concat that takes
+    str1 and str2 as arguments and
+    returns a concatenated string
+
+    """
 
     return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 """
-  function floor which takes a float n as argument and returns the floor of the float.
+  function floor which takes
+  a float n as argument
+  and returns the floor of the float.
 """
 import math
 
 
 def floor(n: float) -> int:
+    """
+    function floor which takes
+    a float n as argument
+    and returns the floor of the float.
+    """
 
     return math.floor(n)
