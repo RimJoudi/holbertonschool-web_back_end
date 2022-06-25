e8b2ec5693c3b982e926314f9cc6c7f76354d5b6
RimJoudi <77971241+RimJoudi@users.noreply.github.com>
2022-06-25 12:45:13 +0100

    Task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""takes an integer max_delay and returns a asyncio.Task"""
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """assyncio.task"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
