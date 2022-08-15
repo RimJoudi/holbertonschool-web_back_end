c20dbe05bed327a8339274f04990d4103b7eeb53
RimJoudi <joudirim93@gmail.com>
2022-08-15 22:14:01 +0100

    fix

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,17 +1,14 @@
 const http = require('http');
 
-const HOSTNAME = '127.0.0.1';
-
-const PORT = 1245;
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School! ');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(PORT, HOSTNAME, () => {
-  /* console.log(`server is running at http://${HOSTNAME}:${PORT}`); */
-});
+app.listen(port, hostname, () => {});
 
 module.exports = app;
