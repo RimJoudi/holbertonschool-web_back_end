84492c8a25731c939b3828b3b318dd2c95d1a715
RimJoudi <joudirim93@gmail.com>
2022-08-15 22:59:29 +0100

    Create a more complex HTTP server using Node's HTTP module

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.end();
+});
+app.listen(port, hostname, () => {});
+module.exports = app;
