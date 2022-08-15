6cac891a58fc26b50d21dcc5cfd2d19d5bd8017a
RimJoudi <joudirim93@gmail.com>
2022-08-15 21:58:49 +0100

    fix

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -2,9 +2,14 @@ const http = require('http');
 
 const PORT = 1245;
 
+const HOSTNAME = '127.0.0.1';
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School! ');
 });
 
-app.listen(PORT);
+app.listen(PORT, HOSTNAME, () => {
+ /* console.log(`server is running at http://${HOSTNAME}:${PORT}`); */
+});
