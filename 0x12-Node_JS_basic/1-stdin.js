52365ed827ab92109f67f5226bb569a88fcb9950
RimJoudi <joudirim93@gmail.com>
2022-08-15 15:19:17 +0100

    fix

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
