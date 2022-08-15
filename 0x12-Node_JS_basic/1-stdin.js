0146ec2655a23a9c66225c043cfb009fa884c393
RimJoudi <joudirim93@gmail.com>
2022-08-15 15:00:15 +0100

    add task 1

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Welcome to Holberton School, what is your name? \n', (name) => {
+  console.log(`Your name is: ${name} `);
+  console.log('This important software is now closing');
+  readline.close();
+});
