1f7c2da87c4233d8ddd912efa2906539e79e7d9d
RimJoudi <joudirim93@gmail.com>
2022-08-16 14:14:52 +0100

    add task 8

---
+++ 0x0E-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,24 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
+/*
+8. No need to create empty objects before adding in properties
+mandatory
+
+Rewrite the getBudgetForCurrentYear function to use ES6 computed property names on the budget object
+
+bob@dylan:~$ npm run dev 8-main.js
+{ 'income-2021': 2100, 'gdp-2021': 5200, 'capita-2021': 1090 }
+
+*/
