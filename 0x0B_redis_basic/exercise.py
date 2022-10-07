15e9aaa541b45329ced72de4f3b043941ab2e615
RimJoudi <joudirim93@gmail.com>
2022-10-07 20:30:42 +0100

    add replay fn

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -52,3 +52,19 @@ class Cache:
     def get_int(self, data: bytes) -> int:
         """ Convert bytes to int """
         return int.from_bytes(data, byteorder)
+    
+
+def replay(method: Callable):
+    """ display the history of calls of a particular function """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+    redis = method.__self__._redis
+    count = redis.get(key).decode("utf-8")
+    print("{} was called {} times:".format(key, count))
+    inputList = redis.lrange(inputs, 0, -1)
+    outputList = redis.lrange(outputs, 0, -1)
+    redis_zipped = list(zip(inputList, outputList))
+    for a, b in redis_zipped:
+        attr, data = a.decode("utf-8"), b.decode("utf-8")
+        print("{}(*{}) -> {}".format(key, attr, data))
