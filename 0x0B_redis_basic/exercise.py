1c099af024a29603fe595b1ec2f576be74a8f3a1
RimJoudi <joudirim93@gmail.com>
2022-10-07 20:35:58 +0100

    fix

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,6 +13,15 @@ def call_history(method: Callable) -> Callable:
     key = method.__qualname__
     inputs = key + ":inputs"
     outputs = key + ":outputs"
+    
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapped function """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
 
 
 def count_calls(method: Callable) -> Callable:
